09f486d8926b693637a07547c044710603632104
Luis Prado <lpradopostigo@gmail.com>
2021-12-29 13:15:06 -0500

    types fixed, updated README

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,5 @@
-/// <reference types="node" />
-export interface GrassAudio {
-    new (): GrassAudio;
+declare module 'grass-audio'{
+  export class GrassAudio {
     setFile(path: string): void;
     setFileFromMemory(buffer: Buffer): void;
     play(): void;
@@ -11,4 +10,8 @@ export interface GrassAudio {
     setVolume(volume: number): void;
     on(event: string, callback: () => void, ...args: any): number;
     once(event: string, callback: () => void, ...args: any): number;
+  }
 }
+
+
+
--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = require("./bin/win/x64/grass_audio.node")
-    .GrassAudio;
+module.exports = require("./bin/win/x64/grass_audio.node");
