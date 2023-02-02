6fa91c847cfafe95e49459706a815de95346d9d1
Luis Prado <lpradopostigo@gmail.com>
2023-02-01 20:42:41 -0500

    add function to convert utf16 to utf8 to header

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,7 @@
 #include "stdint.h"
 
 wchar_t* GA_Utf8ToUtf16(const char* utf8);
+char* GA_Utf16ToUtf8(const wchar_t* utf16);
 uint16_t GA_ResolveIndex(int16_t index, uint16_t size);
 const char* GA_Concat(const char* str1, const char* str2);
 
